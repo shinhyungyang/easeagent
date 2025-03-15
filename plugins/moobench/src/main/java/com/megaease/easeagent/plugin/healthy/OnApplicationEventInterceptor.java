ca29c775ce807080fac50fff9978ee097163082e
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2025-03-15 15:56:56 +0100

    Mockup files for plugin-moobench

---
+++ plugins/moobench/src/main/java/com/megaease/easeagent/plugin/healthy/OnApplicationEventInterceptor.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021, MegaEase
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.megaease.easeagent.plugin.healthy;
+
+import com.megaease.easeagent.plugin.enums.Order;
+import com.megaease.easeagent.plugin.interceptor.Interceptor;
+import com.megaease.easeagent.plugin.interceptor.MethodInfo;
+import com.megaease.easeagent.plugin.annotation.AdviceTo;
+import com.megaease.easeagent.plugin.api.Context;
+import com.megaease.easeagent.plugin.api.health.AgentHealth;
+
+@AdviceTo(value = SpringApplicationAdminMXBeanRegistrarAdvice.class)
+public class OnApplicationEventInterceptor implements Interceptor {
+    @Override
+    public void before(MethodInfo methodInfo, Context context) {
+
+    }
+
+    @Override
+    public void after(MethodInfo methodInfo, Context context) {
+    }
+
+    @Override
+    public String getType() {
+        return "healthReady";
+    }
+
+    @Override
+    public int order() {
+        return Order.HIGH.getOrder();
+    }
+}
